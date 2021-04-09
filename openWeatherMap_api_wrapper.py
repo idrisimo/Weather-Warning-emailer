8e9c15c5dd063a3f4ca34141c62b97361b95ad3f
idrisimo <idrissilva@hotmail.com>
2021-04-10 00:40:37 +0100

    Initial commit.
    This is the MVP.  Gets weather data from api. checks if weather is severe enough then emails it to correct inbox.

---
+++ openWeatherMap_api_wrapper.py
@@ -0,0 +1,41 @@
+import requests
+import logging
+import time
+from datetime import datetime
+import os
+
+
+def get_fiveday_forcast(city_name, api_key):
+    # Modify the individual ranges to alter the severity of the weather type added to the dictionary. See link below:
+    # Weather id list: https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
+    weather_id_list = [
+        [201, 202, 231, 232],
+        [301, 302, 311, 312, 313, 314, 321],
+        [501, 502, 503, 504, 511, 521, 522, 531],
+        [601, 602, 611, 613, 616, 621, 622]
+    ]
+    weather_dict = {}
+    # https://openweathermap.org/forecast5#parameter
+    path = f'http://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={api_key}'
+    session = requests.session()
+    try:
+        response = session.get(path)
+        json_data = response.json()
+        # Break down Json to get date, time, and weather condition
+        for weather_items in json_data['list']:
+            weather_id = int(weather_items['weather'][0]['id'])
+            weather_type = weather_items['weather'][0]['description']
+            weather_datetime = str(datetime.strptime(weather_items['dt_txt'],
+                                                     '%Y-%m-%d %H:%M:%S')).split(' ')
+            weather_dow = datetime.strptime(weather_datetime[0], '%Y-%m-%d')
+            weather_dow = weather_dow.strftime('%A')
+            for weather_id_range in weather_id_list:
+                if weather_id in weather_id_range:
+                    if weather_dow != ['Saturday', 'Sunday']:
+                        weather_dict.setdefault(weather_datetime[0], {'Time of Day': weather_datetime[1],
+                                                 'Weather Type': weather_type})
+    except Exception:
+        time_stamp = time.asctime(time.localtime(time.time()))
+        logging.basicConfig(filename='weather app.log', filemode='w')
+        logging.exception(f'[{time_stamp}]: An error has occurred\n-------------------------')
+    return weather_dict
